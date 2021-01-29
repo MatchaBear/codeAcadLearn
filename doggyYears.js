08fde966ef94584a55e8a3e0ab78964a00e01f77
MatchaBear <matchabear@users.noreply.github.com>
2021-01-30 00:02:56 +0800

    lupa titik koma

--- doggyYears.js
+++ doggyYears.js
@@ -18,3 +18,4 @@ let myAgeInDogYears = earlyYears + laterYears;
 myName = 'Berry'.toLowerCase();
 
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
