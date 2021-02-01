93d5b4b382672aab4339fd5ac423e5ac9e5779a4
MatchaBear <matchabear@users.noreply.github.com>
2021-02-01 22:23:29 +0800

    submitting noobs code

--- liar.js
+++ liar.js
@@ -4,5 +4,13 @@
 let yourName = prompt("What is your name ?");
 while (yourName !== "Berry") {
 	console.log("You are a liar!");
-}
+	break;
+} console.log("Of course you are!");
 
+
+// ori code:
+//let yourName;
+//do {
+//  yourName = prompt("Who are you?");
+//} while (!yourName);
+//console.log(yourName);
